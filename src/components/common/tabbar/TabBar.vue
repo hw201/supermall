3387daec3fc75843336f10039e22ee279f34e364
hw201 <742567969@qq.com>
2021-05-08 17:17:31 +0800

    初始化项目

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar",
+};
+</script>
+
+<style>
+#tab-bar {
+  display: flex;
+  background: rgb(243, 243, 243);
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  font-size: 12px;
+}
+</style>
---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="tab-bar-item" @click="btn">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :style="activeStyle"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    //App的path="/home"
+    path: String,
+
+    activeColor: {
+      type: String,
+      default: "red",
+    },
+  },
+  data() {
+    return {
+      //isActive: true,
+    };
+  },
+
+  computed: {
+    //动态决定isAcitve true/false
+    isActive() {
+      //this.$route.path那个路由处于活跃就哪个对象
+      //判断this.$route.path.包不包含 props的this.path
+      //这个home和item（home）一样就true，则其他就flase,home=》item（home）=true
+      //home=》item（car）=flase
+      //home=》item（classify）=flase
+      //home=》item（profile）=flase
+      //什么时候有这个path ！==-1的时候
+      return this.$route.path.indexOf(this.path) !== -1;
+    },
+    activeStyle() {
+      //动态绑定activeColor，先来上面请求isActive，如果true就 { color: this.activeColor }，false就{}
+      return this.isActive ? { color: this.activeColor } : {};
+    },
+  },
+  methods: {
+    btn() {
+      //每个item和路由对应起来
+      this.$router.push(this.path).catch((err) => err);
+    },
+  },
+};
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+}
+.tab-bar-item img {
+  height: 24px;
+  width: 24px;
+  padding-top: 5px;
+}
+.active {
+  color: red;
+}
+</style>
