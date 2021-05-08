9a513d1ee0cc787ba4f340b7ef531fd73c138e02
hw201 <742567969@qq.com>
2021-05-08 16:00:25 +0800

    初始化项目

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+/* 配置别名 */
+module.exports = {
+  configurewebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+      }
+    }
+  }
+}
